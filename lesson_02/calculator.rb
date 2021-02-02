9bf76da17011d69627119285c81c710a9b3ad28f
Ginni Pinckert <gcpinckert@gmail.com>
2021-02-02 10:52:22 -0800

    Align styles in calculator.rb to Ruby style guide

--- lesson_02/calculator.rb
+++ lesson_02/calculator.rb
@@ -13,7 +13,7 @@ end
 
 def operation_to_message(op)
   case op
-  when '1' 
+  when '1'
     'Adding'
   when '2'
     'Subtracting'
@@ -24,43 +24,43 @@ def operation_to_message(op)
   end
 end
 
-prompt("Welcome to Calculator! Enter your name:")
+prompt "Welcome to Calculator! Enter your name:"
 
 name = ''
 loop do
   name = gets.chomp
 
   if name.empty?
-    prompt("Make sure to use a valid name.")
+    prompt "Make sure to use a valid name."
   else
     break
   end
 end
 
-prompt("Hi, #{name}!")
+prompt "Hi, #{name}!"
 
 loop do
   num1 = '' # initialize outside loop to have outer scope
   loop do
-    prompt("What's the first number?")
+    prompt "What's the first number?"
     num1 = gets.chomp
 
     if valid_number?(num1)
       break
     else
-      prompt("Invalid number") # error message
+      prompt "Invalid number" # error message
     end
   end
 
   num2 = ''
   loop do
-    prompt("What's the second number?")
+    prompt "What's the second number?"
     num2 = gets.chomp
 
     if valid_number?(num2)
       break
     else
-      prompt("Invalid number")
+      prompt "Invalid number"
     end
   end
 
@@ -72,33 +72,33 @@ loop do
     4) divide
   MSG
 
-  prompt(operator_prompt)
+  prompt operator_prompt
 
-  operator= ''
+  operator = ''
   loop do
     operator = gets.chomp
 
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt("Must choose 1, 2, 3, or 4")
+      prompt "Must choose 1, 2, 3, or 4"
     end
   end
 
-  prompt("#{operation_to_message(operator)} the two numbers...")
+  prompt "#{operation_to_message(operator)} the two numbers..."
 
-  result = case operator
-          when "1" then num1.to_i + num2.to_i
-          when "2" then num1.to_i - num2.to_i
-          when "3" then num1.to_i * num2.to_i
-          when "4" then num1.to_f / num2.to_f
-          end
+  result =  case operator
+            when "1" then num1.to_i + num2.to_i
+            when "2" then num1.to_i - num2.to_i
+            when "3" then num1.to_i * num2.to_i
+            when "4" then num1.to_f / num2.to_f
+            end
 
-  prompt("The result is #{result}.")
+  prompt "The result is #{result}."
 
-  prompt("Do you want to perform another calculation? (Y to go again)")
+  prompt "Do you want to perform another calculation? (Y to go again)"
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt("Thank you for using the calculator. Goodbye.")
+prompt "Thank you for using the calculator. Goodbye."
