e9b535e1416a2041af5f050543d9225d26bff1e2
Ginni Pinckert <gcpinckert@gmail.com>
2021-02-12 12:49:34 -0800

    Rock, Paper, Scissors: Add initial functionality

---
+++ lesson_02/rock_paper_scissors.rb
@@ -0,0 +1,44 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def prompt(message)
+  puts "==> #{message}"
+end
+
+def display_result(player, computer)
+  puts "You chose: #{player}; Computer chose: #{computer}"
+  if  (player == 'rock' && computer == 'scissors') ||
+      (player == 'paper' && computer == 'rock') ||
+      (player == 'scissors' && computer == 'paper')
+    prompt("You won!")
+  elsif (player == 'rock' && computer == 'paper') ||
+        (player == 'paper' && computer == 'scissors') ||
+        (player == 'scissors' && computer == 'rock')
+    prompt ("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
+    choice = gets.chomp
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  display_result(choice, computer_choice)
+
+  prompt "Do you want to play again?"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt "Thank you for playing. Goodbye!"
