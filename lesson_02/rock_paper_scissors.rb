23907c01b054cac8f97d4d0aa59ccd141b0f362f
Ginni Pinckert <gcpinckert@gmail.com>
2021-02-12 16:17:19 -0800

    Rock, Paper, Scissors: Add bonus feature 1, lizard and spock

--- lesson_02/rock_paper_scissors.rb
+++ lesson_02/rock_paper_scissors.rb
@@ -1,17 +1,47 @@
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = %w(rock paper scissors lizard spock)
+
+def clear_screen
+  system('clear') || system('cls')
+end
 
 def prompt(message)
-  puts "==> #{message}"
+  puts ">> #{message}"
+end
+
+def display_welcome
+  clear_screen
+  prompt "It's time to play #{VALID_CHOICES.join(' ').upcase}!"
+end
+
+def get_player_choice
+  loop do
+    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
+    choice = gets.chomp
+
+    if VALID_CHOICES.include?(choice)
+      return choice
+    else
+      prompt "That's not a valid choice."
+    end
+  end
+end
+
+def display_choices(choice1, choice2)
+  prompt "You chose: #{choice1}"
+  prompt "Computer chose: #{choice2}"
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  winning_moves = { scissors: %w(paper lizard),
+                    rock: %w(lizard scissors),
+                    paper: %w(rock scissors),
+                    lizard: %w(paper spock),
+                    spock: %w(rock scissors) }
+  key = first.to_sym
+  winning_moves[key].include?(second)
 end
 
 def display_result(player, computer)
-  prompt "You chose: #{player}; Computer chose: #{computer}"
   if win?(player, computer)
     prompt "You won!"
   elsif win?(computer, player)
@@ -21,26 +51,27 @@ def display_result(player, computer)
   end
 end
 
-loop do
-  choice = ''
-  loop do
-    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
-    choice = gets.chomp
-
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt "That's not a valid choice."
-    end
-  end
-
-  computer_choice = VALID_CHOICES.sample
-
-  display_result(choice, computer_choice)
-
-  prompt "Do you want to play again?"
+def play_again?
+  prompt "Do you want to play again? (Enter 'y' to keep playing)"
   answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  answer.downcase == 'y'
 end
 
-prompt "Thank you for playing. Goodbye!"
+def display_goodbye
+  prompt "Thank you for playing. Goodbye!"
+end
+
+display_welcome
+
+loop do
+  player_choice = get_player_choice
+  computer_choice = VALID_CHOICES.sample
+
+  display_choices(player_choice, computer_choice)
+  display_result(player_choice, computer_choice)
+
+  break unless play_again?
+  clear_screen
+end
+
+display_goodbye
