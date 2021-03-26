17ef2c4b85b3a83b7dd5afed07265a11fb251d84
Ginni Pinckert <gcpinckert@gmail.com>
2021-03-26 16:43:53 -0700

    Lesson 6: Add Tic Tac Toe basic functionality

--- lesson_06/tic_tac_toe/problem_decomp.md
+++ lesson_06/tic_tac_toe/problem_decomp.md
@@ -2,11 +2,12 @@
 
 ## Decomposing the Problem
 
-**Game Description**
+### Game Description
+
 Tic Tac Toe is a 2 player game played on a 3x3 board. Each player takes a turn and
 marks a square on the board. First player to reach 3 squares in a row, including diagonals, wins. If all 9 squares are marked and no player has 3 squares in a row, then the game is a tie.
 
-**Game Play**
+### Game Play
 
 1. Display the initial empty 3x3 board.
 2. Ask the user to mark a square
@@ -27,40 +28,37 @@ Inner loop each turn -> #2 - #7
 1. Start
     - Display welcome message
 2. Display Board
-    - Create nested array to represent board `[[_|_|_], [_|_|_], [_|_|_]]
-    - Output to the screen
-    - Create array of available moves
+    - Make sure this is reset on each game play through
+    - Create hash to represent state of board `{1 => ' ', 2 => ' ', ...}`
+      - Each number key in the hash represents a square on the board
+      - Each value represents what is displayed within that square (X, O , or space)
+    - Output the board to the screen
 3. User Marks Square
     - Display prompt to user
     - Get user input
     - Validate user input
-    - Mutate nested board array with player's move
-    - Remove player's move from available moved
+      - Make sure only blank squares are available to the user
+    - Update board with player's move (permanent modification)
     - Display board with user's move
 4. Computer Marks Square
-    - Randomly select move from array of available moves
-    - Remove computer move from available moves
-    - Change nested board array
+    - Randomly select move from array of available moves (`board.keys.select{ |num| brd[num] == INITIAL_MARKER}`)
+    - Update Board (permanent modification)
     - Display board array
 5. Winner?
     - Winning conditions include:
 
     ```ruby
-    # horizontal win:
-    board[0][0] == board[0][1] == board[0][2] ||
-    board[1][0] == board[1][1] == board[1][2] ||
-    board[2][0] == board[2][1] == board[2][2]
-
-    # vertical win:
-    board[0][0] == board[1][0] == board[2][0] ||
-    board[0][1] == board[1][1] == board[2][1] ||
-    board[0][2] == board[1][2] == board[2][2]
-
-    # diagonal win:
-    board[0][0] == board[1][1] == board[2][2] ||
-    board[0][2] == board[1][1] == board[2][0]
+    # Each number in the sub-arrays represents a square
+    # Each number is also a key in the hash representing the state of the board
+    winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                    [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                    [[1, 5, 9], [3, 5, 7]]              # diagonals
     ```
 
+    - Check to see if the values are equal across any of the winning lines
+      - Iterate over the nested array, for each sub-array
+      - Check the value of each item within the sub array against the other items
+    - Break out of the method with a return statement within a conditional
     - If no, check if board is full
     - If yes, display winner
     - Winner depends on whether char is `x` or `o`
@@ -74,3 +72,19 @@ Inner loop each turn -> #2 - #7
     - Validate input
     - If yes, back to #2
     - If no, display goodbye message
+
+## Other Notes
+
+### Rubocop
+
+If you find that a cop is complaining about something that you feel is necessary in the code (for example, the length of a method that is displaying something like an image to the screen), you can disable it using a comment:
+
+`# rubocop:disable <NameOfCopHere>`
+
+You then re-enable that particular cop again after the section of code that you didn't want it active.
+
+`# rubocop:enable <NameOfCop>`
+
+Additionally, you can disable / enable as many cops as need be with a single comment.
+
+`# rubocop:disable <NameOfCop>, <AnotherCop>`
---
+++ lesson_06/tic_tac_toe/tictactoe.rb
@@ -0,0 +1,153 @@
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                [[1, 5, 9], [3, 5, 7]]              # diagonals
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+# rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+def display_welcome
+  loop do
+    system 'clear'
+    puts ""
+    puts " _____ _        _____            _____"
+    puts "|_   _(_) ___  |_   _|_ _  ___  |_   _|__   ___"
+    puts "  | | | |/ __|   | |/ _` |/ __|   | |/ _ \\ / _ \\"
+    puts "  | | | | (__    | | (_| | (__    | | (_) |  __/"
+    puts "  |_| |_|\\___|   |_|\\__,_|\\___|   |_|\\___/ \\___|"
+    puts ""
+    puts ""
+    prompt "Welcome to Tic Tac Toe!"
+    prompt "Your goal: beat the Tic-Tac-Toeminator."
+    prompt "Get three X's in a row, horizontal, vertical, or diagonal."
+    prompt "Are you ready to save humanity from the tyranny of the O's?"
+    prompt "Press 'y' to begin!"
+    answer = gets.chomp.downcase
+    break if answer == 'y'
+  end
+end
+# rubocop:enable Metrics/AbcSize, Metrics/MethodLength
+
+# rubocop:disable Metrics/AbcSize
+def display_board(brd)
+  system 'clear'
+  puts "You are #{PLAYER_MARKER}. Tic-Tac-Toeminator is #{COMPUTER_MARKER}."
+  puts ""
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+# rubocop:enable Metrics/AbcSize
+
+# Generates hash in which key is a integer representing square on the board
+# Value tells what to display in the board when output (X, O, or blank square)
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  new_board
+end
+
+# Returns an array of integers representing available moves
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+# Player's turn
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    square = gets.chomp.to_i
+    if empty_squares(brd).include?(square)
+      break
+    else
+      prompt "Sorry, that's not a valid choice"
+    end
+  end
+
+  brd[square] = PLAYER_MARKER
+end
+
+# Computer's turn: random
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+# Determines if there is a tie
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+# Determines if the game is over
+def someone_won?(brd)
+  !!detect_winner(brd) # If there is a winner, true, if nil, false
+end
+
+# Determines who the winner is, if any
+def detect_winner(brd)
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return "You"
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return "Tic-Tac-Toeminator"
+    end
+  end
+
+  nil
+end
+
+def turn_cycle(brd)
+  loop do
+    display_board(brd)
+    player_places_piece!(brd)
+    break if someone_won?(brd) || board_full?(brd)
+    computer_places_piece!(brd)
+    break if someone_won?(brd) || board_full?(brd)
+  end
+
+  display_board(brd)
+end
+
+def game_over(brd)
+  if someone_won?(brd)
+    prompt "#{detect_winner(brd)} won!"
+  else
+    prompt "It's a tie!"
+  end
+end
+
+def play_game
+  loop do
+    board = initialize_board
+
+    turn_cycle(board)
+    game_over(board)
+
+    prompt "Play again? (y or n)"
+    answer = gets.chomp
+    break unless answer.downcase.start_with?('y')
+  end
+end
+
+def display_goodbye
+  prompt "Thanks, hero, for your daring deeds!"
+  prompt "Goodbye until the Tic-Tac-Toeminator strikes again!"
+end
+
+display_welcome
+play_game
+display_goodbye
